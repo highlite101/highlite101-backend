06b9c246e8d5aaa5e1c4d30916c819c77f56fbb3
highlite101 <ireyefojumoses@gmail.com>
2025-07-25 05:30:34 +0100

    Delete sample.js

--- sample.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json({ message: 'Welcome to Highlite101 API' });
-});
-
-module.exports = router;
