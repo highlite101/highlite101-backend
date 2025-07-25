272898d8be9e39bc18d8fa849b2a9feb54c9a1bf
highlite101 <ireyefojumoses@gmail.com>
2025-07-25 05:30:05 +0100

    Delete db.js

--- db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://db_highlite101:db_Yemiybnl16@cluster0.czpf3aa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.log("❌ MongoDB connection error:", err));
